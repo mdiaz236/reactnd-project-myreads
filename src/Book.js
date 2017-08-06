5b079d7a93f37e6c49285f6a06f6180b73635da7
Miguel Diaz <mdiaz7.10.2@gmail.com>
2017-08-06 10:55:40 -0400

    Add Bookshelf component and create initial static version

--- src/Book.js
+++ src/Book.js
@@ -1,24 +1,22 @@
 import React from 'react'
 
 const Book = (props) => (
-  <li>
-    <div className="book">
-      <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url(${props.url})` }}></div>
-        <div className="book-shelf-changer">
-          <select>
-            <option value="none" disabled>Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
-          </select>
-        </div>
+  <div className="book">
+    <div className="book-top">
+      <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url(${props.data.imageLinks.smallThumbnail})` }}></div>
+      <div className="book-shelf-changer">
+        <select value={props.data.shelf}>
+          <option value="none" disabled>Move to...</option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
       </div>
-      <div className="book-title">{props.title}</div>
-      <div className="book-authors">{props.author}</div>
     </div>
-  </li>
+    <div className="book-title">{props.data.title}</div>
+    <div className="book-authors">{props.data.authors.join(', ')}</div>
+  </div>
 )
 
 export default Book
